d3c7d42a76f25dcd368d7c83f6dac174fa3ed728
SF <sf2023@github.com>
2024-04-21 23:28:48 -0500

    BMI lab

---
+++ lab-10-bmi/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import BodyMassIndexForm from './components/BodyMassIndexForm.vue'
+</script>
+
+<template>
+  <div>
+
+
+  </div>
+  <BodyMassIndexForm msg="Body Mass Index Form" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ lab-10-bmi/src/components/BodyMassIndexForm.vue
@@ -0,0 +1,37 @@
+<script setup>
+import {h, ref} from 'vue'
+
+defineProps({
+  //extra credit needs this
+
+})
+//define the messages this component may send to other components
+const emit = defineEmits([
+    'stats-entered' // we can pick the names
+    //this message gets sent when the user
+    //enters their data and app should do math
+])
+const height = ref('')
+const weight = ref('')
+function statsEntered(){
+//   send the data to app.vue to do the math and display the data
+  emit('stats-entered', height)
+  emit('stats-entered', weight)
+  //goes to this component's parent
+  //where does this message go? App.vue
+}
+
+</script>
+
+<template>
+  <label>Height in meters</label>
+  <input v-model="height">
+  <label>Weight in Kilos</label>
+  <input v-model="weight">
+  <button v-on:click ="statsEntered">Calculate</button>
+
+</template>
+
+<style scoped>
+
+</style>
